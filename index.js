56a1b214e6c64ce2cee751a868b24227aa5edbf9
Dhruv278 <dhruvgopani8@gmail.com>
2023-11-06 12:55:42 +0530

    final changes

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = socketio(server);
 const path=require('path');
-app.use(express.static(path.join(__dirname,'client/build')))
+app.use(express.static(path.join('client/build')))
 app.use('*',(req,res)=>{
     console.log(__dirname)
-    console.log(path.join(__dirname,'client/build'))
+    console.log(path.join('client/build'))
     res.sendFile(path.resolve('client/build/index.html'))
 })
 
