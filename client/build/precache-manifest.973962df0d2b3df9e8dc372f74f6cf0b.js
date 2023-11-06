b203201f39890a8c60c310a118ddc43b031264fe
Dhruv278 <dhruvgopani8@gmail.com>
2023-11-06 12:58:09 +0530

    add build

---
+++ client/build/precache-manifest.973962df0d2b3df9e8dc372f74f6cf0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69b43545f59ec25943a2e2301587d1d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcf5f79aed0c4fe05c34",
+    "url": "/static/css/main.01ca2eba.chunk.css"
+  },
+  {
+    "revision": "a28de3fc9f4250e1d562",
+    "url": "/static/js/2.6a93ae1d.chunk.js"
+  },
+  {
+    "revision": "bcf5f79aed0c4fe05c34",
+    "url": "/static/js/main.354ccf2c.chunk.js"
+  },
+  {
+    "revision": "b95af7ab8beceaf41336",
+    "url": "/static/js/runtime~main.2bbd149a.js"
+  }
+]);
