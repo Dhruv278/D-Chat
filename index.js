0732a585fd36b4fefd1fdb8be1832f258e1f9d25
Dhruv278 <dhruvgopani8@gmail.com>
2023-11-06 12:39:53 +0530

    'changes'

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ io.on('connection', (socket) => {
      
         io.to(user.room).emit('roomData',{room:user.room, users:getUserInRoom(user.room)})
         callback();
-
+// add data
  })
 
     socket.on('sendMessage',(message,callback)=>{
